fd925a9c01ca0707c650efe7f2d2a6c00f9f9b41
kreadyf <info@kreadyf.com>
2020-05-06 13:11:37 -0400

    config

--- projects/kdyf-auth-angular/src/public-api.ts
+++ projects/kdyf-auth-angular/src/public-api.ts
@@ -2,6 +2,19 @@
  * Public API Surface of kdyf-auth-angular
  */
 
-export * from './lib/kdyf-auth-angular.service';
-export * from './lib/kdyf-auth-angular.component';
-export * from './lib/kdyf-auth-angular.module';
+// Models
+export * from './lib/models/auth.grant-type.enum';
+export * from './lib/models/auth.models';
+export * from './lib/models/auth-config.model';
+
+// Services
+export * from './lib/services/auth.service';
+export * from './lib/services/auth-guard.service';
+export * from './lib/services/auth-http-interceptor.service';
+
+// NGRX
+export * from './lib/auth.actions';
+export * from './lib/auth.effects';
+export * from './lib/auth.meta-reducer';
+export * from './lib/auth.module';
+export * from './lib/auth.reducer';
