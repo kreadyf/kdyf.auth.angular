913b578725b3ef8f3606a4751620b7518bcbb370
kreadyf <info@kreadyf.com>
2020-05-07 14:58:20 -0400

    merge with master

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('kdyf-auth app is running!');
+    expect(page.getTitleText()).toEqual('libraries app is running!');
   });
 
   afterEach(async () => {
