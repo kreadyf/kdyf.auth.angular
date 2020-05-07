b4806a0a96451ec8ec5d85c5eb3cb448f7306d1c
kreadyf <info@kreadyf.com>
2020-05-07 14:51:14 -0400

    update

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# KdyfAuth
+# Libraries
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.1.0.
 
--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/kdyf-auth'),
+      dir: require('path').join(__dirname, './coverage/libraries'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
